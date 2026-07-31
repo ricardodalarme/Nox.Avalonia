4792bd9ce39a09bec3b41252f43599008e33546d
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-07-31 11:12:12 -0300

    feat: better align DataGrid and PropertGrid themes

--- src/Nox.Avalonia.PropertyGrid/NoxPropertyGridTheme.axaml.cs
+++ src/Nox.Avalonia.PropertyGrid/NoxPropertyGridTheme.axaml.cs
@@ -1,13 +1,41 @@
 using System;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.Styling;
 
 namespace Nox.Avalonia.PropertyGrid;
 
 public class NoxPropertyGridTheme : Styles
 {
+    static NoxPropertyGridTheme()
+    {
+        Control.LoadedEvent.AddClassHandler<global::Avalonia.PropertyGrid.Controls.PropertyGrid>((pg, e) =>
+        {
+            ApplyHeaderFontFix(pg);
+        });
+    }
+
     public NoxPropertyGridTheme(IServiceProvider? sp = null)
     {
         AvaloniaXamlLoader.Load(sp, this);
+    }
+
+    private static void ApplyHeaderFontFix(global::Avalonia.PropertyGrid.Controls.PropertyGrid pg)
+    {
+        var columnName = pg.FindControl<TextBlock>("ColumnName");
+        if (columnName != null)
+        {
+            columnName.FontSize = 12;
+            columnName.FontWeight = FontWeight.SemiBold;
+        }
+
+        var columnProperties = pg.FindControl<TextBlock>("ColumnProperties");
+        if (columnProperties != null)
+        {
+            columnProperties.FontSize = 12;
+            columnProperties.FontWeight = FontWeight.SemiBold;
+        }
     }
 }
